8bb4d6e2ab9d5c9a1621b0d13354c6deeb5b7ec5
AndreyLS <andreyj1591@gmail.com>
2020-02-24 23:23:25 -0500

    Dibujando con turtle

---
+++ 01-Turtle/python.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+tAndrey = turtle.Turtle()
+
+tAndrey.forward(70)
+tAndrey.right(70)
+tAndrey.forward(100)
+tAndrey.left(180)
+tAndrey.forward(200)
+tAndrey.left(140)
+tAndrey.forward(200)
+
+
+turtle.mainloop()
