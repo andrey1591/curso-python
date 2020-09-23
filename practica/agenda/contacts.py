8435a87568d7713c97c86dc658fa10cd9a7dcde8
andrey1591 <30941271+andrey1591@users.noreply.github.com>
2020-09-23 15:58:20 -0500

    Agenda

--- practica/agenda/contacts.py
+++ practica/agenda/contacts.py
@@ -2,9 +2,9 @@
 
 class Contact:
     def __init__(self, name, phone, email):
-        self._name = name
-        self._phone = phone
-        self._email = email
+        self.name = name
+        self.phone = phone
+        self.email = email
 
 class ContactBook:
     def __init__(self):
@@ -13,6 +13,43 @@ class ContactBook:
     def add(self, name, phone, email):
         contact = Contact(name, phone, email)
         self._contacts.append(contact)
+
+
+    def show_all(self):
+        for contact in self._contacts:
+            self._print_contact(contact)
+
+    def delete(self, name):
+        for idx, contact in enumerate(self._contacts):
+            if contact.name.lower() == name.lower():
+                del self._contacts[idx]
+                break
+
+    def update(self, name):
+        for contant in self._contacts:
+            if contant.name.lower() == name.lower():
+                break
+
+    def search(self, name):
+        for contact in self._contacts:
+            if contact.name.lower() == name.lower():
+                self._print_contact(contact)
+                break
+        else:
+            self._not_found()
+
+
+    def _print_contact(self, contact):
+        print('--- * --- * --- * --- * --- * --- * ---')
+        print('Nombre: {}'.format(contact.name))
+        print('Nombre: {}'.format(contact.phone))
+        print('Email: {}'.format(contact.email))
+        print('--- * --- * --- * --- * --- * --- * ---')
+
+    def _not_found(self):
+        print('************************')
+        print('¡Contacto no encontrado!')
+        print('************************')
        
 def run():
 
@@ -33,23 +70,26 @@ def run():
 
 
         if command == 'a':
-            name = str(input('Escribe el nombre del contacto'))
-            phone = str(input('Escribe el número de telefono'))
-            email = str(input('Escribe el email de contacto'))
+            name = str(input('Escribe el nombre del contacto: '))
+            phone = str(input('Escribe el número de telefono: '))
+            email = str(input('Escribe el email de contacto: '))
 
             contact_book.add(name, phone, email)
         
         elif command == 'ac':
-            print('Actualizar contacto')
+            name = str(input('Escribir el nombre de contacto: '))
+            contact_book.update(name)
         
         elif command == 'b':
-            print('Buscar contacto')
+            name = str(input('Escribir el nombre de contacto: '))
+            contact_book.search(name)
 
         elif command == 'e':
-            print('Eliminar contacto')
+            name = str(input('Escribir el nombre de contacto: '))
+            contact_book.delete(name)
 
         elif command == 'l':
-            print('Listar contactos')
+            contact_book.show_all()
 
         elif command == 's':
             break
