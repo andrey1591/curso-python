1e82fdf00dd232908867dd07588802c19f7c825b
andrey1591 <30941271+andrey1591@users.noreply.github.com>
2020-09-25 15:12:11 -0500

    Manejo de rutas y logica con Flask

--- servidor/main.py
+++ servidor/main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from contact_model import Contact
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    return render_template('index.html')
 
-def hello_world():
-    return 'Bienvenidos al Himalaya!'
+
+
+@app.route(r'/add', methods=['GET', 'POST'])
+def add_contact():
+    if request.form:
+        contact = Contact(name=request.form.get('name'), phone=request.form.get('phone'), email=request.form.get('email'))
+        contact.put()
+    return render_template('add_contact.html')
 
 
 if __name__ == '__main__':
